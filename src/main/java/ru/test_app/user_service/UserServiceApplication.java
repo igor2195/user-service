5ceaee31999bf8cc648ec9f71da146ec44bddb7c
Igor Nejivlyak <Igoryan818@yandex.ru>
2026-04-13 22:56:50 +0300

    убрал тест кэш

--- src/main/java/ru/test_app/user_service/UserServiceApplication.java
+++ src/main/java/ru/test_app/user_service/UserServiceApplication.java
@@ -2,13 +2,12 @@ package ru.test_app.user_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
 }
